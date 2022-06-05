1bd327536735fb9b0500d889efe409f3fca8fca0
Hamzh Freijat <hamzhg2016@gmail.com>
2022-06-05 14:42:02 +0300

    Done

--- README.md
+++ README.md
@@ -3,32 +3,14 @@ You can navigate through three page home , add task and all task page .
 
 ![Home Page](screenshots/home.png)
 
-After update you can now add you name from setting page then it will appear in the home and you can navigate throug the tasks 
+![login Page](screenshots/login-page.png)
 
-![Home Page](screenshots/Screenshot2.png)
+![signUP Page](screenshots/signUP.png)
 
+![add_task Page](screenshots/add_task.png)
 
-## Recycle view 
-In this time I add recycleview to show the task in the main page 
+![task_details Page](screenshots/task_details.png)
 
-![recycleView](screenshots/recycleView.png)
-
-## Room 
-Now you can add the task to your sqlite database from add task button 
-
-![recycleView](screenshots/home-page.png)
-![recycleView](screenshots/add_task.png)
-![recycleView](screenshots/task_details.png)
+![gallary Page](screenshots/gallary.png)
 
 
-## AWS 
-Now the app is connect with AWS backend and the task is related to the team thats belongs to . 
-
-![AWS](screenshots/Screenshot_1.png)
-![AWS](screenshots/Screenshot_2.png)
-![AWS](screenshots/Screenshot_3.png)
-
-
-## Authentication system 
-![signUP](screenshots/signUP.png)
-![login-page](screenshots/login-page.png)
--- app/src/main/java/com/example/taskmaster/ui/TaskDetailActivity.java
+++ app/src/main/java/com/example/taskmaster/ui/TaskDetailActivity.java
@@ -10,6 +10,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,42 +20,46 @@ import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 import com.amplifyframework.datastore.generated.model.Task;
+import com.bumptech.glide.Glide;
 import com.example.taskmaster.R;
 import com.example.taskmaster.data.AppDatabase;
 import com.example.taskmaster.data.TaskData;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
+    private static final String TAG = TaskDetailActivity.class.getSimpleName();
     private String title , desc , status;
+    private ImageView imageView;
+    private String imageKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_task_detail);
+
         TextView textTitle = findViewById(R.id.text_title);
         TextView textDesc = findViewById(R.id.text_description);
         TextView textStatus = findViewById(R.id.text_status);
+
+        imageView = findViewById(R.id.img_task);
+
         Intent intent = getIntent();
 
-//        try {
-//            Amplify.addPlugin(new AWSDataStorePlugin());
-//            Amplify.addPlugin(new AWSApiPlugin());
-//            Amplify.configure(getApplicationContext());
-//
-//            Log.i("Tutorial", "Initialized Amplify");
-//        } catch (AmplifyException e) {
-//            Log.e("Tutorial", "Could not initialize Amplify", e);
-//        }
 
         Handler handler = new Handler(Looper.getMainLooper() , msg -> {
 
             textTitle.setText(title);
             textDesc.setText(desc);
             textStatus.setText(status.toString());
+            if ( imageKey != null){
+                setImage(imageKey);
+            }
+
             return true ;
         });
 
@@ -67,6 +72,7 @@ public class TaskDetailActivity extends AppCompatActivity {
                         title = response.getData().getTitle();
                         desc = response.getData().getBody();
                         status = response.getData().getStatus();
+                        imageKey = response.getData().getImage();
 
 
 
@@ -85,5 +91,24 @@ public class TaskDetailActivity extends AppCompatActivity {
 
 //        TaskData taskData = AppDatabase.getInstance(this).taskDao().getTaskByID(Long.valueOf(intent.getStringExtra("id")));
 
+    }
+
+    private void setImage(String image) {
+        if(image != null) {
+            Amplify.Storage.downloadFile(
+                    image,
+                    new File(getApplicationContext().getFilesDir() + "/" + image + "download.jpg"),
+                    result -> {
+                        Log.i(TAG, "The root path is: " + getApplicationContext().getFilesDir());
+                        Log.i(TAG, "Successfully downloaded: " + result.getFile().getName());
+                        runOnUiThread(() -> {
+
+                            Glide.with(this).load(result.getFile().getPath()).into(imageView);
+
+                        });
+                    },
+                    error -> Log.e(TAG, "Download Failure", error)
+            );
+        }
     }
 }
